578fc5487a79a6434c0db447ab37911f44d6235d
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-23 19:10:08 +0900

    tasklet/HelloTasklet1.javaを作成する

--- src/main/java/com/udemy/hello_spring_batch_2/tasklet/HelloTasklet1.java
+++ src/main/java/com/udemy/hello_spring_batch_2/tasklet/HelloTasklet1.java
@@ -1,5 +1,10 @@
 package com.udemy.hello_spring_batch_2.tasklet;
 
+import org.springframework.stereotype.Component;
+
+//@Componentアノテーションによって、Spring Beanとして登録される
+
+@Component
 public class HelloTasklet1 {
 
 }
