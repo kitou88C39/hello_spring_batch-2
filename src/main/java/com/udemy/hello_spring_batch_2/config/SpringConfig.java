af5d7aad0177b6e19e921b4345c97f3ebc4e2aee
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-23 13:47:28 +0900

    config/SpringConfig.javaを作成する

--- src/main/java/com/udemy/hello_spring_batch_2/config/SpringConfig.java
+++ src/main/java/com/udemy/hello_spring_batch_2/config/SpringConfig.java
@@ -1,5 +1,10 @@
 package com.udemy.hello_spring_batch_2.config;
 
+import org.springframework.context.annotation.Configuration;
+
 public class SpringConfig {
+
+    // @Configurationアノテーションを付与する
+    @Configuration
 
 }
