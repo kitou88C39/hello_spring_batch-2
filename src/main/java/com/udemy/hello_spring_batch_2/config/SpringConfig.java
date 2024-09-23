3ce16d95dfb88d74c1288ac9adb0ae6ef9a3715d
kitoumasaomi <kanfutrooper@gmail.com>
2024-09-23 13:51:07 +0900

    config/SpringConfig.javaを作成する

--- src/main/java/com/udemy/hello_spring_batch_2/config/SpringConfig.java
+++ src/main/java/com/udemy/hello_spring_batch_2/config/SpringConfig.java
@@ -1,10 +1,16 @@
 package com.udemy.hello_spring_batch_2.config;
 
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 public class SpringConfig {
 
     // @Configurationアノテーションを付与する
     @Configuration
+    private final JobLauncher jobLauncher;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
 
 }
